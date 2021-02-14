1de623089fe45153d9968734628efc46dfb0b232
justin761002 <justin22923178@gmail.com>
2021-02-14 19:00:58 +0800

    Add Swagger(springfox)

--- src/main/java/com/justin761002/invoice/controller/InvoiceController.java
+++ src/main/java/com/justin761002/invoice/controller/InvoiceController.java
@@ -3,6 +3,9 @@ package com.justin761002.invoice.controller;
 import com.justin761002.invoice.dto.CarrierAggregate;
 import com.justin761002.invoice.dto.WinningListResponse;
 import com.justin761002.invoice.service.InvoiceService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Invoice")
 @RestController
 @RequestMapping("/eInvoice")
 public class InvoiceController {
@@ -18,8 +22,11 @@ public class InvoiceController {
     @Autowired
     InvoiceService invoiceService;
 
+    @ApiOperation(value = "查詢中獎發票號碼清單", notes = "輸入民國年月（偶數月）")
     @GetMapping("/winningList")
-    public ResponseEntity<WinningListResponse> queryWinningList(@RequestParam String yyyMM) {
+    public ResponseEntity<WinningListResponse> queryWinningList(
+            @ApiParam(value = "輸入民國年月（ex: 10906）")
+            @RequestParam String yyyMM) {
         return invoiceService.queryWinningList(yyyMM);
     }
 
--- src/main/java/com/justin761002/invoice/dto/WinningListResponse.java
+++ src/main/java/com/justin761002/invoice/dto/WinningListResponse.java
@@ -1,38 +1,105 @@
 package com.justin761002.invoice.dto;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @lombok.Data
+@ApiModel("中獎發票號碼清單")
 public class WinningListResponse {
+    @ApiModelProperty(value = "版本號碼", example = "0.2")
     private String v;
+
+    @ApiModelProperty(value = "訊息回應碼", example = "200")
     private String code;
+
+    @ApiModelProperty(value = "系統回應訊息",example = "查詢成功")
     private String msg;
+
+    @ApiModelProperty(value = "查詢開獎期別(yyyyMM)", example = "10106")
     private String invoYm;
+
+    @ApiModelProperty(value = "千萬特獎號碼", example = "29734661")
     private String superPrizeNo;
+
+    @ApiModelProperty(value = "特獎號碼", example = "99028391")
     private String spcPrizeNo;
+
+    @ApiModelProperty(value = "特獎號碼 2")
     private String spcPrizeNo2;
+
+    @ApiModelProperty(value = "特獎號碼 3")
     private String spcPrizeNo3;
+
+    @ApiModelProperty(value = "頭獎號碼 1", example = "91180939")
     private String firstPrizeNo1;
+
+    @ApiModelProperty(value = "頭獎號碼 2", example = "12753377")
     private String firstPrizeNo2;
+
+    @ApiModelProperty(value = "頭獎號碼 3", example = "22638178")
     private String firstPrizeNo3;
+
+    @ApiModelProperty(value = "頭獎號碼 4")
     private String firstPrizeNo4;
+
+    @ApiModelProperty(value = "頭獎號碼 5")
     private String firstPrizeNo5;
+
+    @ApiModelProperty(value = "頭獎號碼 6")
     private String firstPrizeNo6;
+
+    @ApiModelProperty(value = "頭獎號碼 7")
     private String firstPrizeNo7;
+
+    @ApiModelProperty(value = "頭獎號碼 8")
     private String firstPrizeNo8;
+
+    @ApiModelProperty(value = "頭獎號碼 9")
     private String firstPrizeNo9;
+
+    @ApiModelProperty(value = "頭獎號碼 10")
     private String firstPrizeNo10;
+
+    @ApiModelProperty(value = "六獎號碼 1", example = "258")
     private String sixthPrizeNo1;
+
+    @ApiModelProperty(value = "六獎號碼 2", example = "066")
     private String sixthPrizeNo2;
+
+    @ApiModelProperty(value = "六獎號碼 3")
     private String sixthPrizeNo3;
-    private String superPrizeAmt;
-    private String spcPrizeAmt;
-    private String firstPrizeAmt;
-    private String secondPrizeAmt;
-    private String thirdPrizeAmt;
-    private String fourthPrizeAmt;
-    private String fifthPrizeAmt;
-    private String sixthPrizeAmt;
+
+    @ApiModelProperty(value = "六獎號碼 4")
     private String sixthPrizeNo4;
+
+    @ApiModelProperty(value = "六獎號碼 5")
     private String sixthPrizeNo5;
+
+    @ApiModelProperty(value = "六獎號碼 6")
     private String sixthPrizeNo6;
+
+    @ApiModelProperty(value = "千萬特獎金額", example = "10000000")
+    private String superPrizeAmt;
+
+    @ApiModelProperty(value = "特獎金額", example = "2000000")
+    private String spcPrizeAmt;
+
+    @ApiModelProperty(value = "頭獎金額", example = "0200000")
+    private String firstPrizeAmt;
+
+    @ApiModelProperty(value = "二獎金額", example = "0040000")
+    private String secondPrizeAmt;
+
+    @ApiModelProperty(value = "三獎金額", example = "0010000")
+    private String thirdPrizeAmt;
+
+    @ApiModelProperty(value = "四獎金額", example = "0004000")
+    private String fourthPrizeAmt;
+
+    @ApiModelProperty(value = "五獎金額", example = "0001000")
+    private String fifthPrizeAmt;
+
+    @ApiModelProperty(value = "六獎金額", example = "0000200")
+    private String sixthPrizeAmt;
 }
