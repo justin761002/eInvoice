079dc1b6c4bd50e6c35ea8af2d2d9da60c8f70f4
justin761002 <justin22923178@gmail.com>
2021-05-06 12:08:34 +0800

    查詢中獎發票號碼清單(winningList)，先讀取 DB 資料，沒有的話呼叫 API 並且回寫進 Database.

--- src/main/java/com/justin761002/invoice/service/InvoiceService.java
+++ src/main/java/com/justin761002/invoice/service/InvoiceService.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.justin761002.invoice.dao.InvoiceDAO;
 import com.justin761002.invoice.dto.CarrierAggregateResponse;
 import com.justin761002.invoice.dto.WinningListResponse;
+import com.justin761002.invoice.persistence.model.WinningListEntity;
+import com.justin761002.invoice.persistence.repository.WinningListRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
@@ -18,12 +22,45 @@ public class InvoiceService {
     @Autowired
     private InvoiceDAO invoiceDAO;
 
+    @Autowired
+    private WinningListRepository winningListRepository;
+
     /**
      * 查詢中獎發票號碼清單
      * @param invTerm 民國年月
      */
     public ResponseEntity<WinningListResponse> queryWinningList(String invTerm) {
-        return invoiceDAO.queryWinningList(invTerm);
+        ResponseEntity<WinningListResponse> response;
+        Optional<WinningListEntity> optionalResult = winningListRepository.findById(invTerm);
+        if(optionalResult.isPresent()) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonStr = objectMapper.writeValueAsString(optionalResult.get());
+                WinningListResponse winningListResponse = objectMapper.readValue(jsonStr, WinningListResponse.class);
+                winningListResponse.setV("0.N");
+                winningListResponse.setCode("200");
+                winningListResponse.setMsg("查詢成功 by PostgreSQL Database");
+                response = ResponseEntity.ok().body(winningListResponse);
+            } catch (JsonProcessingException e) {
+                logger.error(e.toString());
+                response = ResponseEntity.notFound().build();
+            }
+       } else {
+            response = invoiceDAO.queryWinningList(invTerm);
+            WinningListResponse winningListResponse = response.getBody();
+            if(winningListResponse != null && "200".equals(winningListResponse.getCode())) {
+                try {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String jsonStr = objectMapper.writeValueAsString(winningListResponse);
+                    logger.info(jsonStr);
+                    WinningListEntity winningListEntity = objectMapper.readValue(jsonStr, WinningListEntity.class);
+                    winningListRepository.save(winningListEntity);
+                } catch (JsonProcessingException e) {
+                    logger.error(e.toString());
+                }
+            }
+        }
+        return response;
     }
 
     /**
